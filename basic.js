d4e93f0b783e59ea8832afe63cb531ea2d408d5a
Natalia <nvolentova@gmail.com>
2020-09-21 10:57:38 +0200

    basic

--- basic.js
+++ basic.js
@@ -1,16 +1,9 @@
+var randomNum = Math.floor(Math.random() * 25) - 5;
 
-getRandomNumber(int min, int max, int rand) {
-	int min=-5;
-	int max=25;
-	var rand="";
-	var rand=((Math.random() * (max - min)) + min);
-    return rand;
-}
-
-if (rand=>5 && rand<10){
-	console.log("The weather is cold and has " + rand + "degrees")
+if (randomNum=>5 && randomNum<10){
+	console.log("The weather is cold and has " + randomNum + " egrees")
 } 
 else {
-	console.log("The weather is moderate" + rand + "degrees")
+	console.log("The weather is moderate" + randomNum + " degrees")
 }
 
